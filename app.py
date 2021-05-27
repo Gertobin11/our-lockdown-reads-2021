cba2507ade7a1ecf79a0753358a56b75d5aa1ae6
Gertobin11 <gertobin11@gmail.com>
2021-05-27 23:55:08 +0000

    added registration form and stylings and checked responsiveness

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import (
     Flask, flash, render_template, redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,11 @@ mongo = PyMongo(app)
 def home():
     books = list(mongo.db.books.find())
     return render_template("index.html", books=books)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    return render_template("register.html")
 
 
 if __name__ == "__main__":

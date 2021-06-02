fadaec0031ad45dd71bba4bb455d15e3cb97af97
Gertobin11 <gertobin11@gmail.com>
2021-06-02 18:51:23 +0000

    added jquery back button to display page

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,8 @@ $(document).ready(function(){
     $('select').formSelect();
   });
 
+  // function to add a back button at the bottom of the display book page
+  // inspiration from https://stackoverflow.com/questions/31347882/back-button-with-javascript-jquery
+$( "#back-btn" ).click(function() {
+  window.history.back();
+});

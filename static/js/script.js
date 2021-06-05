5388d35241b9cba3a68ecbcc3a7522457802c590
Gertobin11 <gertobin11@gmail.com>
2021-06-05 22:57:44 +0000

    added a default image to url as I encounteed a problem loading image dynamically

--- static/js/script.js
+++ static/js/script.js
@@ -12,9 +12,9 @@ $( "#back-btn" ).click(function() {
 });
 
 // function to replace cover image with the default logo if link is broken
-// code got from 
+// code got from https://css-tricks.com/snippets/jquery/better-broken-image-handling/
 
 $("img").on("error", function () {
-    $(this).attr("src", "static/images/our-lockdown-reads-logo-reworked.png");
+    $(this).attr("src", "https://media.istockphoto.com/vectors/old-book-cover-vector-id1094620944?k=6&m=1094620944&s=612x612&w=0&h=fEqo1TbdxYj3aGqpC0aMqsYx8DPGN7RzZAn6pdO08QM=");
 });
 

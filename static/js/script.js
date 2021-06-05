0dc641b67b651095a06567790a805397c8b0b7c0
Gertobin11 <gertobin11@gmail.com>
2021-06-05 22:36:01 +0000

    added backup image for failed image loads

--- static/js/script.js
+++ static/js/script.js
@@ -5,8 +5,16 @@ $(document).ready(function(){
     $('.modal').modal();
   });
 
-  // function to add a back button at the bottom of the display book page
-  // inspiration from https://stackoverflow.com/questions/31347882/back-button-with-javascript-jquery
+// function to add a back button at the bottom of the display book page
+// inspiration from https://stackoverflow.com/questions/31347882/back-button-with-javascript-jquery
 $( "#back-btn" ).click(function() {
   window.history.back();
 });
+
+// function to replace cover image with the default logo if link is broken
+// code got from 
+
+$("img").on("error", function () {
+    $(this).attr("src", "static/images/our-lockdown-reads-logo-reworked.png");
+});
+
